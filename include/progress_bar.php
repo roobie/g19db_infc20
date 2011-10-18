f5112a7fd79188e6a2f4c5d216d36bfc50987df7
Björn Roberg <bjorn.roberg@gmail.com>
2011-10-18 12:58:04 +0200

    deleted:    README 	deleted:    create_database.php 	deleted:    create_student.php 	deleted:    include/header_no_sidebar.php 	deleted:    include/header_sidebar.php 	deleted:    include/progress_bar.php

--- include/progress_bar.php
+++
@@ -1,8 +0,0 @@
-<?php
-	function progressBar($percentage) {
-		print "<div id=\"progress-bar\" class=\"all-rounded\">\n";
-		print "<div id=\"progress-bar-percentage\" class=\"all-rounded\" style=\"width: $percentage%\">";
-			if ($percentage > 5) {print "$percentage%";} else {print "<div class=\"spacer\">&nbsp;</div>";}
-		print "</div></div>";
-	}
-?>
