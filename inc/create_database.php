e5061a182fb54e096ced2d67d5f276feaa4818b9
Björn Roberg <bjorn.roberg@gmail.com>
2011-11-03 23:51:18 +0100

    PDO stuffs

--- inc/create_database.php
+++ inc/create_database.php
@@ -1,179 +1,179 @@
 <?php
 	
 	require 'database_props.php';
-	
-	mysql_connect('localhost',$user,$password);
-	@mysql_select_db($database) or die( "Unable to select database");
-	
-	//====================================================================
-	// TABLE DROPS && DEFS
-	//====================================================================
-	
-	//--- BEGIN:	DROPS ---
-	
-	$query="DROP TABLE IF EXISTS assignments";
-	mysql_query($query);
-	
-	$query="DROP TABLE IF EXISTS section";
-	mysql_query($query);
-	
-	$query="DROP TABLE IF EXISTS course_requirements";
-	mysql_query($query);
-	
-	$query="DROP TABLE IF EXISTS studies";
-	mysql_query($query);
-	
-	$query="DROP TABLE IF EXISTS has_studied";
-	mysql_query($query);
-	
-	$query="DROP TABLE IF EXISTS course";
-	mysql_query($query);
-	
-	$query="DROP TABLE IF EXISTS student";
-	mysql_query($query);
-	
-	//--- END:		DROPS ---
-	
-	//--------------------------------------------------------------------
-	
-	//--- BEGIN:	CREATES ---
-	
-	$query="";
-	mysql_query($query);
 
-	//---
-	
-	$query="CREATE  TABLE IF NOT EXISTS `g19db`.`student` (
-			  `idstudent` INT NOT NULL AUTO_INCREMENT ,
-			  `social_security_number` VARCHAR(12) NULL ,
-			  `first_name` VARCHAR(45) NOT NULL ,
-			  `last_name` VARCHAR(45) NOT NULL ,
-			  `address` VARCHAR(255) NOT NULL ,
-			  `phone_number` VARCHAR(45) NOT NULL ,
-			  `email` VARCHAR(45) NOT NULL ,
-			  `type` VARCHAR(45) NOT NULL ,
-			  PRIMARY KEY (`idstudent`) ,
-			  UNIQUE INDEX `social_security_number_UNIQUE` (`social_security_number` ASC) )
-			ENGINE = InnoDB";
-	mysql_query($query);
+	try {
+		$db = new PDO($pdo_connection_string, $user, $password);
 
-	//---
-	
-	$query="CREATE  TABLE IF NOT EXISTS `g19db`.`course` (
-			  `idcourse` INT NOT NULL AUTO_INCREMENT ,
-			  `code` VARCHAR(6) NOT NULL ,
-			  `name` VARCHAR(45) NOT NULL ,
-			  `points` VARCHAR(5) NOT NULL ,
-			  PRIMARY KEY (`idcourse`) )
-			ENGINE = InnoDB";
-	mysql_query($query);
-	
-	//---
-	
-	$query="CREATE  TABLE IF NOT EXISTS `g19db`.`section` (
-			  `idsection` INT NOT NULL ,
-			  `idcourse` INT NOT NULL ,
-			  `name` VARCHAR(45) NOT NULL ,
-			  PRIMARY KEY (`idsection`, `idcourse`) ,
-			  INDEX `fk_course_section` (`idcourse` ASC) ,
-			  CONSTRAINT `fk_course_section`
-			    FOREIGN KEY (`idcourse` )
-			    REFERENCES `g19db`.`course` (`idcourse` )
-			    ON DELETE NO ACTION
-			    ON UPDATE NO ACTION)
-			ENGINE = InnoDB";
-	mysql_query($query);
-	
-	//---
-	
-	$query="CREATE  TABLE IF NOT EXISTS `g19db`.`assignments` (
-			  `idassigments` INT NOT NULL COMMENT '	' ,
-			  `idsection` INT NOT NULL ,
-			  `name` VARCHAR(45) NULL ,
-			  `description` VARCHAR(45) NULL ,
-			  PRIMARY KEY (`idassigments`, `idsection`) ,
-			  INDEX `fk_section_assignment` (`idsection` ASC) ,
-			  CONSTRAINT `fk_section_assignment`
-			    FOREIGN KEY (`idsection` )
-			    REFERENCES `g19db`.`section` (`idsection` )
-			    ON DELETE NO ACTION
-			    ON UPDATE NO ACTION)
-			ENGINE = InnoDB";
-	mysql_query($query);
-	
-	//---
-	
-	$query="CREATE  TABLE IF NOT EXISTS `g19db`.`course_requirements` (
-			  `idcourse` INT NOT NULL ,
-			  `idcourse_required` INT NOT NULL ,
-			  PRIMARY KEY (`idcourse`, `idcourse_required`) ,
-			  INDEX `fk_course` (`idcourse` ASC) ,
-			  INDEX `fk_course_required` (`idcourse_required` ASC) ,
-			  CONSTRAINT `fk_course`
-			    FOREIGN KEY (`idcourse` )
-			    REFERENCES `g19db`.`course` (`idcourse` )
-			    ON DELETE NO ACTION
-			    ON UPDATE NO ACTION,
-			  CONSTRAINT `fk_course_required`
-			    FOREIGN KEY (`idcourse_required` )
-			    REFERENCES `g19db`.`course` (`idcourse` )
-			    ON DELETE NO ACTION
-			    ON UPDATE NO ACTION)
-			ENGINE = InnoDB";
-	mysql_query($query);
-	
-	//---
-	
-	$query="CREATE  TABLE IF NOT EXISTS `g19db`.`studies` (
-			  `idstudent` INT NOT NULL ,
-			  `idcourse` INT NOT NULL ,
-			  PRIMARY KEY (`idstudent`, `idcourse`) ,
-			  INDEX `fk_student_studies` (`idstudent` ASC) ,
-			  INDEX `fk_course_studies` (`idcourse` ASC) ,
-			  CONSTRAINT `fk_student_studies`
-			    FOREIGN KEY (`idstudent` )
-			    REFERENCES `g19db`.`student` (`idstudent` )
-			    ON DELETE NO ACTION
-			    ON UPDATE NO ACTION,
-			  CONSTRAINT `fk_course_studies`
-			    FOREIGN KEY (`idcourse` )
-			    REFERENCES `g19db`.`course` (`idcourse` )
-			    ON DELETE NO ACTION
-			    ON UPDATE NO ACTION)
-			ENGINE = InnoDB";
-	mysql_query($query);
-	
-	//---
-	
-	$query="CREATE  TABLE IF NOT EXISTS `g19db`.`has_studied` (
-			  `idstudent` INT NOT NULL ,
-			  `idcourse` INT NOT NULL ,
-			  `grade` VARCHAR(45) NOT NULL ,
-			  PRIMARY KEY (`idstudent`, `idcourse`) ,
-			  INDEX `fk_student_has_studied` (`idstudent` ASC) ,
-			  INDEX `fk_course_has_studied` (`idcourse` ASC) ,
-			  CONSTRAINT `fk_student_has_studied`
-			    FOREIGN KEY (`idstudent` )
-			    REFERENCES `g19db`.`student` (`idstudent` )
-			    ON DELETE NO ACTION
-			    ON UPDATE NO ACTION,
-			  CONSTRAINT `fk_course_has_studied`
-			    FOREIGN KEY (`idcourse` )
-			    REFERENCES `g19db`.`course` (`idcourse` )
-			    ON DELETE NO ACTION
-			    ON UPDATE NO ACTION)
-			ENGINE = InnoDB";
-	mysql_query($query);
-	
-	//--- END:		CREATES ---
-	
-	//====================================================================
-	
-	mysql_close();
+		//====================================================================
+		// TABLE DROPS && DEFS
+		//====================================================================
+		
+		//--- BEGIN:	DROPS ---
+		
+		$db->query("DROP TABLE IF EXISTS assignments");
+		
+
+		$db->query("DROP TABLE IF EXISTS section");
+		
+		
+		$db->query("DROP TABLE IF EXISTS course_requirements");
+		
+		
+		$db->query("DROP TABLE IF EXISTS studies");
+		
+		
+		$db->query("DROP TABLE IF EXISTS has_studied");
+		
+		
+		$db->query("DROP TABLE IF EXISTS course");
+		
+		
+		$db->query("DROP TABLE IF EXISTS student");
+		
+		
+		//--- END:		DROPS ---
+		
+		//--------------------------------------------------------------------
+		
+		$db->query("CREATE  TABLE IF NOT EXISTS `g19db`.`student` (
+				  `idstudent` INT NOT NULL AUTO_INCREMENT ,
+				  `social_security_number` VARCHAR(12) NULL ,
+				  `first_name` VARCHAR(45) NOT NULL ,
+				  `last_name` VARCHAR(45) NOT NULL ,
+				  `address` VARCHAR(255) NOT NULL ,
+				  `phone_number` VARCHAR(45) NOT NULL ,
+				  `email` VARCHAR(45) NOT NULL ,
+				  `type` VARCHAR(45) NOT NULL ,
+				  PRIMARY KEY (`idstudent`) ,
+				  UNIQUE INDEX `social_security_number_UNIQUE` (`social_security_number` ASC) )
+				ENGINE = InnoDB");
+		
+
+		//---
+		
+		$db->query("CREATE  TABLE IF NOT EXISTS `g19db`.`course` (
+				  `idcourse` INT NOT NULL AUTO_INCREMENT ,
+				  `code` VARCHAR(45) NULL COMMENT '	' ,
+				  `name` VARCHAR(45) NULL ,
+				  PRIMARY KEY (`idcourse`) )
+				ENGINE = InnoDB");
+		
+		
+		//---
+		
+		$db->query("CREATE  TABLE IF NOT EXISTS `g19db`.`section` (
+				  `idsection` INT NOT NULL ,
+				  `idcourse` INT NOT NULL ,
+				  `name` VARCHAR(45) NOT NULL ,
+				  PRIMARY KEY (`idsection`, `idcourse`) ,
+				  INDEX `fk_course_section` (`idcourse` ASC) ,
+				  CONSTRAINT `fk_course_section`
+				    FOREIGN KEY (`idcourse` )
+				    REFERENCES `g19db`.`course` (`idcourse` )
+				    ON DELETE NO ACTION
+				    ON UPDATE NO ACTION)
+				ENGINE = InnoDB");
+		
+		
+		//---
+		
+		$db->query("CREATE  TABLE IF NOT EXISTS `g19db`.`assigments` (
+				  `idassigments` INT NOT NULL COMMENT '	' ,
+				  `idsection` INT NOT NULL ,
+				  `name` VARCHAR(45) NULL ,
+				  `description` VARCHAR(45) NULL ,
+				  PRIMARY KEY (`idassigments`, `idsection`) ,
+				  INDEX `fk_section_assignment` (`idsection` ASC) ,
+				  CONSTRAINT `fk_section_assignment`
+				    FOREIGN KEY (`idsection` )
+				    REFERENCES `g19db`.`section` (`idsection` )
+				    ON DELETE NO ACTION
+				    ON UPDATE NO ACTION)
+				ENGINE = InnoDB");
+		
+		
+		//---
+		
+		$db->query("CREATE  TABLE IF NOT EXISTS `g19db`.`course_requirements` (
+				  `idcourse` INT NOT NULL ,
+				  `idcourse_required` INT NOT NULL ,
+				  PRIMARY KEY (`idcourse`, `idcourse_required`) ,
+				  INDEX `fk_course` (`idcourse` ASC) ,
+				  INDEX `fk_course_required` (`idcourse_required` ASC) ,
+				  CONSTRAINT `fk_course`
+				    FOREIGN KEY (`idcourse` )
+				    REFERENCES `g19db`.`course` (`idcourse` )
+				    ON DELETE NO ACTION
+				    ON UPDATE NO ACTION,
+				  CONSTRAINT `fk_course_required`
+				    FOREIGN KEY (`idcourse_required` )
+				    REFERENCES `g19db`.`course` (`idcourse` )
+				    ON DELETE NO ACTION
+				    ON UPDATE NO ACTION)
+				ENGINE = InnoDB");
+		
+		
+		//---
+		
+		$db->query("CREATE  TABLE IF NOT EXISTS `g19db`.`studies` (
+				  `idstudent` INT NOT NULL ,
+				  `idcourse` INT NOT NULL ,
+				  PRIMARY KEY (`idstudent`, `idcourse`) ,
+				  INDEX `fk_student_studies` (`idstudent` ASC) ,
+				  INDEX `fk_course_studies` (`idcourse` ASC) ,
+				  CONSTRAINT `fk_student_studies`
+				    FOREIGN KEY (`idstudent` )
+				    REFERENCES `g19db`.`student` (`idstudent` )
+				    ON DELETE NO ACTION
+				    ON UPDATE NO ACTION,
+				  CONSTRAINT `fk_course_studies`
+				    FOREIGN KEY (`idcourse` )
+				    REFERENCES `g19db`.`course` (`idcourse` )
+				    ON DELETE NO ACTION
+				    ON UPDATE NO ACTION)
+				ENGINE = InnoDB");
+		
+		
+		//---
+		
+		$db->query("CREATE  TABLE IF NOT EXISTS `g19db`.`has_studied` (
+				  `idstudent` INT NOT NULL ,
+				  `idcourse` INT NOT NULL ,
+				  `grade` VARCHAR(45) NOT NULL ,
+				  PRIMARY KEY (`idstudent`, `idcourse`) ,
+				  INDEX `fk_student_has_studied` (`idstudent` ASC) ,
+				  INDEX `fk_course_has_studied` (`idcourse` ASC) ,
+				  CONSTRAINT `fk_student_has_studied`
+				    FOREIGN KEY (`idstudent` )
+				    REFERENCES `g19db`.`student` (`idstudent` )
+				    ON DELETE NO ACTION
+				    ON UPDATE NO ACTION,
+				  CONSTRAINT `fk_course_has_studied`
+				    FOREIGN KEY (`idcourse` )
+				    REFERENCES `g19db`.`course` (`idcourse` )
+				    ON DELETE NO ACTION
+				    ON UPDATE NO ACTION)
+				ENGINE = InnoDB");
+		
+		
+		//--- END:		CREATES ---
+		
+		//====================================================================
+
+		// RELEASE THE connection
+		$db = null;
+	} catch (PDOException $e) {
+		print "Error!: " . $e->getMessage() . "<br/>";
+		echo "Error!: " . $e->getMessage() . "<br/>";
+		$error = $e;
+		
+		header("Location: application.php?error=" . $error->getMessage() );
+	}
 	
 	// Redirects to the referring page.
 	if (header("Location: ".$_SERVER["HTTP_REFERER"]) == 'localhost') {
-			header("Location: ".$_SERVER["HTTP_REFERER"]);
+		header("Location: ".$_SERVER['HTTP_REFERER']);
 	}
 	
 
--- inc/create_student.php
+++ inc/create_student.php
@@ -7,40 +7,48 @@
 	$phone_nbr = mysql_escape_string($_POST["phone_nbr"]);
 	$email = mysql_escape_string($_POST["email"]);
 	$type = mysql_escape_string($_POST["type"]);
-	
-	require 'database_props.php';
-	
-	mysql_connect('localhost',$user,$password);
-	@mysql_select_db($database) or die( "Unable to select database");
-	
-	$query="INSERT INTO student VALUES (null, '$ssn', '$fname', '$lname', '$address', '$phone_nbr', '$email', '$type')";
-	mysql_query($query);
+
+	$db = null;
+	$data = array($ssn, $fname, $lname, $address, $phone_nbr, $email, $type);
+	$query="INSERT INTO student VALUES (null, ?, ?, ?, ?, ?, ?, ?)";
+
+	try {
+		require 'database_props.php';
+		
+		$db = new PDO($pdo_connection_string, $user, $password);
+
+		$stmt = $db->prepare($query);
+
+		$db->beginTransaction();
+		$stmt->execute($data);
+
+		$last_id = $db->query('SELECT LAST_INSERT_ID() as last_id');
+		$last_id->setFetchMode(PDO::FETCH_ASSOC);
+		$last_id = $last_id->fetch();
+		$last_id = intval($last_id['last_id']);
+				
+		$query_check = "SELECT idstudent FROM student WHERE idstudent='$last_id'";
+		
+		$stmt = $db->query($query_check);
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		foreach ($stmt->fetch() as $row) {
+			if(!$row['idstudent'] == $last_id) { // problem
+				echo '<div class="ui-state-error">Operation failed, please try again.</div>';
+				$db->rollBack();
+			} else {
+				echo '<div class="ui-state-highlight">OK - student no. ' . $last_id . ' added to database</div>';
+			}
+		}
+		
+		$db->commit();
+		
+		$db = null;
+	} catch (PDOException $e) {
+		echo '<div class="ui-state-error">FAIL. Try again or consult the webmaster.</div> Append the following info in the message, please: ' . date_default_timezone_set('l jS \of F Y h:i:s A') . $e->getMessage();
+	}
 	
 	require 'applib.inc';
 	
-	append_sql($query);
+	append_text('../files/sql_log.txt', $query);
 	
-	$query_check = "SELECT * FROM student WHERE
-					first_name = '$fname' and
-					last_name = '$lname' and
-					address = '$address' and
-					phone_number = '$phone_nbr' and
-					email = '$email' and
-					type = '$type' or
-					social_security_number = '$ssn'
-					";
-	
-	$result = mysql_query($query_check);
-	
-	if (!$result) {
-		echo '<div class="ui-state-error">FAIL. Try again or consult the webmaster.</div> Append the following info in the message, please: ' . date('l jS \of F Y h:i:s A');
-	} else {
-		$row = mysql_fetch_assoc($result);
-		if ($row["first_name"] == "$fname") {
-			echo 'OK - student added to database';
-		} else {
-			echo '<div class="ui-state-error">FAIL - student not added to database. Please try again...</div>';;
-		}
-	}	
-	mysql_close();
 ?>
