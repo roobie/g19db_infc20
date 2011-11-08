8c1c77ba9919fede4c69bef77a93f82651cdaeb7
Erik Samuelsson <durkslag@gmail.com>
2011-11-08 14:29:34 +0100

    added some JS 2 SQL

---
+++ includes/app/scripts/remove_course.php
@@ -0,0 +1,47 @@
+<?php
+
+	$idcourse = mysql_escape_string($_POST["idcourse"]);
+
+echo $idcourse; 
+
+	$db = null;
+	$data = array(idcourse);
+	$query="CALL RemoveCourse(?)";
+
+	try {
+		require 'database_props.php';
+
+		$db = new PDO($pdo_connection_string, $user, $password);
+		$stmt = $db->prepare($query);
+		$db->beginTransaction();
+		$stmt->execute($data);
+
+		$db->commit();
+
+		$check_stmt = $db->prepare("CALL GetCourseByID(?)");
+		$db->beginTransaction();
+		$check_stmt->execute($data);
+
+		$db->commit();
+
+		$result = $check_stmt->fetchAll();
+		$count = 0;
+
+		foreach ($result as $row) {
+			if ($row != null){
+				$count ++;
+				throw new PDOException("Course not removed. Constraint failed");
+			}
+		}
+
+		if ($count == 0 ) {
+			echo "Course successfully removed from database!";
+		}
+
+		$db = null;
+	} 
+	
+	catch (PDOException $e) {
+		echo '<div class="ui-state-error">FAIL. ' . $e->getMessage() . '</div>';
+	}
+?>
